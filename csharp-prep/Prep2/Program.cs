ad35730252c2bd0e5b41206ad297ac01969acc2f
Caden Baird <bai22005@byui.edu>
2024-01-13 23:22:53 -0700

    W01 Part1 & Part2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percent = Console.ReadLine();
+        int answer = int.Parse(percent);
+
+        string letter = "";
+
+        if (answer >= 90)
+        {
+                letter = "A";
+        }
+        
+        else if (answer >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (answer >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (answer >=60)
+        {
+            letter = "D";
+        }
+
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (answer >= 70 )
+        {
+        Console.Write("Congratulations you passed!");
+        }
+
+        else 
+        {
+            Console.WriteLine("You failed. Better luck next time!!");
+        }
     }
 }
