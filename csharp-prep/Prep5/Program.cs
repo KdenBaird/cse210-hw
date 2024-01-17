77aaa606ddf51cfda1b0f8768994e16ab5669ce1
Caden Baird <bai22005@byui.edu>
2024-01-17 15:05:16 -0700

    Finished Assignment #5.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        string userName = UserName();
+        int userNumber = UserNumber();
+        int squareNumber = SquareNumber(userNumber);
+         DisplayResult(userName, squareNumber);
+
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the Program! ");
+        }
+        
+        
+        static string UserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name =  Console.ReadLine();
+            
+            return name;
+        }
+
+        static int UserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+        
+        static int SquareNumber(int number)
+        {
+            int squareNumber = number * number;
+
+            return squareNumber;
+        }
+        static void DisplayResult(string name, int number)
+        {
+            Console.Write($"{name}, the square of your number is {number}");
+        }
     }
 }
