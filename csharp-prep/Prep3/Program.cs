51484bdb9f65978b5eceb847e5d0f41c23794a61
Caden Baird <bai22005@byui.edu>
2024-01-17 13:43:26 -0700

    Finished Assignment #3 and #4.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Part2:
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+       while (guess != magicNumber)
+       {
+        Console.Write("What is your guess?");
+         guess = int.Parse(Console.ReadLine());
+
+        if (guess < magicNumber)
+            {
+            Console.WriteLine("Higher");
+            
+            }
+        else if (guess > magicNumber) {
+            Console.WriteLine("Lower");
+            }
+        else 
+            {
+            Console.WriteLine("You guessed it! ");
+            }
+       }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+            int response = -1;
+            Console.WriteLine("Write a list of numbers, enter 0 when finished. ");
+            while (response != 0)
+            {
+                Console.Write("Enter number: ");
+        
+
+
+         response = int.Parse(Console.ReadLine());
+         
+            if (response != 0) 
+            {
+                numbers.Add(response);          
+            }
+
+            int sum = 0;
+            float average = 0;
+            int max = numbers.Max();
+            foreach (int number in numbers) 
+            {
+            sum += number;
+            average = sum / numbers.Count;
+            }
+            
+            if (response == 0) 
+            {
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+            }
+        }
     }
 }
