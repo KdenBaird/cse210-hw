1c69ec169b7dee49dc90d617b0759dc69708d5b8
Caden Baird <bai22005@byui.edu>
2024-01-24 12:42:44 -0700

    progress

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1.company = "Del Taco";
+        job1.jobTitle = "Manager";
+        job1.startYear = 1706;
+        job1.endYear = 2020;
+        
+        
+
+        Job job2 = new Job();
+        job2.company = "Google";
+        job2.jobTitle = "CTO";
+        job2.startYear = 1706;
+        job2.endYear = 2020;
+        
+        Resume r = new Resume();
+        r.name = "Kden Baird";
+
+        r.job.Add(job1);
+        r.job.Add(job2);
+        r.Display();
     }
 }
